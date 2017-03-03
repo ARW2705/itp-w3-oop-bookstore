235259be8144351c2a1d5e0d0e4f76eb68df266c
Andrew Wanex <awanex2705@gmail.com>
2017-03-03 00:55:48 +0000

    Initial commit

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,10 +1,38 @@
 class Bookstore(object):
-    pass
+    def __init__(self, name):
+        self.name = name
+        self.library = []
+        
+    def get_books(self):
+        return self.library
+        
+    def add_book(self, book):
+        self.library.append(book)
+        
+    def search_books(self, title = None, author = None):
+        search_return = []
+        for item in self.library:
+            if author is not None:
+                if author.name in item.author.name:
+                    search_return.append(item)
+            elif title is not None:
+                if title.lower() in item.title.lower():
+                    search_return.append(item)
+        return search_return
 
 
 class Author(object):
-    pass
+    def __init__(self, name, nationality):
+        self.name = name
+        self.nationality = nationality
+        self.library = []
+        
+    def get_books(self):
+        return self.library
 
 
 class Book(object):
-    pass
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        author.library.append(self)
